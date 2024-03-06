05f3d9b5854ed1ef78d4bbe50e0b4cae3b8b742c
Topaz712 <tzam712@gmail.com>
2024-03-06 03:59:21 -0600

    ADD: route to upload an image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_one_attached :image
+
+  def upload_image
+    user = User.find(params[:user_id])
+
+    if user.image.attach(params[:image])
+      render json: { message: "Image uploaded" }, status: :ok
+    else
+      render json: { message: "Image upload failed"}, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :create]
+  resources :users do
+    post 'upload_image', to: 'users#upload_image'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
